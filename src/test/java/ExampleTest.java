7d3f178015202459c83a8c3037d129cf79379fee
a.anait <airiian.anait@depositphotos.com>
2018-04-05 22:22:19 +0300

    add gitignore and test

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,14 +1,13 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ExampleTest {
 
     @Test
-
-    public void test ()
-
-    {
+    public void test () {
         // step by step
-
+        System.out.println("My first test");
+        Assert.assertEquals("2+2 is not equal to 4", 3, 2+2);
 
     }
 }
