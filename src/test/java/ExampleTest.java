69f36b37f49c99b9422a2c2a6a48fdb25614d58e
a.anait <airiian.anait@depositphotos.com>
2018-04-19 19:34:36 +0300

    Exercise:chapterTree

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,13 +1,36 @@
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class ExampleTest {
 
     @Test
-    public void test () {
-        // step by step
-        System.out.println("My first test");
-        Assert.assertEquals("2+2 is not equal to 4", 3, 2+2);
-
+    public void canAddTwoPlusTwo(){
+        int answer = 2+2;
+        assertEquals("2+2=4", 4, answer);
     }
+
+    @Test
+    public void canSubtractTwoMinusTwo () {
+        int answer = 2-2;
+        assertEquals( "2-2=0",0, answer);
+    }
+
+    @Test
+    public void canDivideTwoDivideTwo () {
+        int answer = 2/2;
+        assertEquals( "2/2=1", 1, answer);
+    }
+
+    @Test
+    public void canMultiplyTwoMultiplyTwo () {
+        int answer = 2*2;
+        assertEquals( "2*2=4", 4, answer);
+    }
+
 }
+
+
+
+
