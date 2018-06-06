8d685b3304d0b6a938f8da0aceadff3bd1cb3f1c
a.anait <airiian.anait@depositphotos.com>
2018-06-06 17:46:38 +0300

    Exercise of chap005: Convert Static usage to Static Import

--- src/main/java/javafortesters/domainobject/TestAppEnv.java
+++ src/main/java/javafortesters/domainobject/TestAppEnv.java
@@ -2,7 +2,11 @@ package javafortesters.domainobject;
 
 public class TestAppEnv {
 
+
+    public static final String DOMAIN = "127.0.0.1";
+    public static final String PORT = "4723";
+
     public static String getUrl() {
-        return "http://127.0.0.1:4723";
+        return "http://" + DOMAIN + ":" + PORT;
     }
 }
--- src/test/java/javafortesters/chapter005withourownclasses/domainobject/examples/TestAppEnvironment.java
+++ src/test/java/javafortesters/chapter005withourownclasses/domainobject/examples/TestAppEnvironment.java
@@ -1,15 +1,31 @@
 package javafortesters.chapter005withourownclasses.domainobject.examples;
 
-import javafortesters.domainobject.TestAppEnv;
+
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static javafortesters.domainobject.TestAppEnv.*;
+
+;
+
 
 public class TestAppEnvironment {
 
     @Test
     public void canGetUrlStatically () {
-        assertEquals ("Return correct URL", "http://127.0.0.1:4723", TestAppEnv.getUrl());
-        System.out.println("Return URL is " + TestAppEnv.getUrl());
+        Assert.assertEquals("Return correct URL", "http://127.0.0.1:4723", getUrl());
+        System.out.println("Return URL is " + getUrl());
+    }
+
+    @Test
+    public void canGetDomain (){
+       Assert.assertEquals("Return DOMAIN use import static", "127.0.0.1", DOMAIN);
+        System.out.println("Return DOMAIN use import of Assert " + DOMAIN);
+    }
+
+    @Test
+    public void canGetPort (){
+        Assert.assertEquals("Return PORT use import static", "4723", PORT);
+        System.out.println("Return PORT use import static with decelerated constant field from another package " + PORT);
     }
 }
